2d612e6b6136045e5b4a90457a63d8951ab02acf
Vlada992 <vladanng@gmail.com>
2019-04-30 16:55:50 +0200

    adding new functionalities, create post

--- src/App.js
+++ src/App.js
@@ -1,60 +1,134 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-import './App.css';
-import List from './components/List/List';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-
+import List from "./components/List/List";
+import Create from "./components/Create/Create";
 
 class App extends Component {
-  constructor(props){
-    console.log('creation phase [constructor] [FIRST CALLED] -1')
-    super(props)
+  constructor(props) {
+    console.log("creation phase [constructor] [FIRST CALLED] -1");
+    super(props);
+
     this.state = {
-      loadedData: null
+      loadedData: null,
+      createTypeDiv:false,
+      createInpDiv:false,
+      placeholder: '',
+      inpValTitle:'',
+      inpValUrl:''
+    };
+  }
+
+  componentDidMount() {
+    console.log("creation phase [componentDidMount] [FOURTH CALLED] -4");
+    axios
+      .get("http://localhost:3004/posts")
+      .then(response => {
+        const respData = response.data;
+        this.setState({ loadedData: respData });
+        console.log(response.data.title);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+
+
+
+  createBtnHandler = () => {
+    console.log('func works');
+    this.setState({createTypeDiv:true})
+  };
+
+
+
+  createInpHandler = (event) => {
+    console.log('event je:', event.target.value)
+    const {value} = event.target;
+      this.setState({
+        placeholder : value,
+        createInpDiv:true,
+        inpValTitle:'',
+        inpValUrl:''
+      })
+  };
+
+
+
+  sendInpVal = (event) => {
+    console.log('48 line:', event.target.value)
+    const {value, name} = event.target;
+    this.setState({
+      [name]: value
+    });
+
+
+    //}
+  };
+
+  createPost = () => {
+    if(this.state.loadedData != null){
+      //
+      const newPost = {
+        id: this.state.loadedData.length + 1,
+        type:'IMAGE',
+        meta: {url: this.state.inpValUrl},
+        title: this.state.inpValTitle
+      }
+      console.log(newPost);
+      //const loadedDataNew = [...this.state.loadedData]
+      //loadedDataNew.push(newPost);
+
+      this.setState( (state) => {
+        state.loadedData = [...this.state.loadedData, newPost];
+        return state;
+    });
+      console.log('loadedData original:', this.state);
+      //
     }
   }
 
-  componentDidMount(){
-    console.log('creation phase [componentDidMount] [FOURTH CALLED] -4');
-
-    axios.get('http://localhost:3004/posts')
-  .then(response => {
-    this.setState({loadedData: response.data}) //loadedData filled with data, here.
-    console.log(response.data.title);
-  })
-  .catch(error => {
-    console.log(error);
-  });
-
-  };
-
-  //axios.get.then.catch;
 
 
-  render(){
-    console.log('creation phase [render] [THIRD CALLED] -3')
-    console.log(this.state.loadedData);
-
+  render() {
+    console.log("creation phase [render] [THIRD CALLED] -3");
     return (
       <div className="container-fluid">
+      
+        <div className="row justify-content-center row-own-0">
+          <div className="col-lg-12 text-center mb-5">
+            <h1>Manage lists</h1>
+          </div>
 
-      <div className='row'>
-      <div className='col-lg-12 text-center'>
-      { this.state.loadedData ?  <List {...this.state}/> : null}
+          <Create  {...this.state}
+           createBtn = {this.createBtnHandler}
+           createInp = {this.createInpHandler}
+           takeInpVal={this.sendInpVal}
+           createPost={this.createPost}
+          />
+        </div>
+
+        <div className="row row-own-1">
+          <div className="col-lg-12 text-center">
+            {this.state.loadedData ? <List {...this.state} /> : null}
+          </div>
+        </div>
+
       </div>
-
-      </div>
-
-
-
-      </div>
-    )
+    );
   }
 }
- 
-
 
 export default App;
+
+
+
+
+
+
+
 
 
 
@@ -67,4 +141,4 @@ export default App;
 //So, turn everything into this.state
 //Or, turn everything into React hooks
 //So, that's the important thing
-//
+//most important things
---
+++ src/components/Create/Create.js
@@ -0,0 +1,43 @@
+import React from "react";
+import '../Create/Create.css';
+
+
+const Create = (props) => {
+    console.log('props u crate:', props.sendInpVal);
+    let placeholderTitle = `Add ${props.placeholder} title`;
+    let placeholderUrl =   `Add ${props.placeholder} url`;
+ 
+  return (
+    <>
+      <div className="col-lg-12">
+        <div className='cont-div'>
+
+            <div className='create-btn-cont'>
+                <button onClick={props.createBtn} className='create-btn'>Create new post</button>
+            </div>
+
+            {props.createTypeDiv ? <div className='create-type-cont'>
+             <button value='Image' onClick={props.createInp}>IMAGE</button>
+             <button value='Video' onClick={props.createInp}>VIDEO</button>
+             <button value='Link' onClick={props.createInp}>LINK</button>
+            </div> : null }
+
+            {props.createInpDiv ? <div>
+                <input type='text' name='inpValTitle' onChange={(e) => props.takeInpVal(e)} value={props.inpValTitle}  placeholder={placeholderTitle}/>
+                <input type='text' name='inpValUrl' onChange={(e) => props.takeInpVal(e)} value={props.inpValUrl} placeholder={placeholderUrl}/>
+                <button  onClick={props.createPost}>Create!</button>
+
+           </div> : null}
+
+
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+
+
+export default Create;
+
